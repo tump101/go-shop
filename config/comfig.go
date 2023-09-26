267663bcf8d9cb987590ebf372499beb1d077c38
tump101 <tump0152@gmail.com>
2023-09-26 15:54:48 +0700

    config.go add app-db-jwt

--- config/comfig.go
+++ config/comfig.go
@@ -1,7 +1,50 @@
 package config
 
-import "fmt"
+import (
+	"os"
+	"time"
+)
 
-func config() {
-	fmt.Println("config")
+func envPath() string {
+	if len(os.Args) == 1 {
+		return ".env"
+	} else {
+		return os.Args[1]
+	}
+}
+
+type config struct {
+	app *app
+	db  *db
+	jwt *jwt
+}
+
+type app struct {
+	host         string
+	port         int
+	name         string
+	verion       string
+	readTimeout  time.Duration
+	writeTimeout time.Duration
+	bodyLimit    int //bytes
+	fileLimit    int //bytes
+	gcpbucket    string
+}
+
+type db struct {
+	db             string
+	port           int
+	protocol       string
+	username       string
+	password       string
+	database       string
+	maxConnections int
+}
+
+type jwt struct {
+	adminKey         string
+	secerKey         string
+	apiKey           string
+	accessExpiresAt  int //bytes
+	refreshExpiresAt int //bytes
 }
